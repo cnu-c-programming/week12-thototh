fb3fd301610cbfea8b69f1cddce51fbba9276d28
thototh <144813512+thototh@users.noreply.github.com>
2026-05-22 11:24:59 +0900

    Update 1-4.c

---
+++ code/1-4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void cat(const char* filename){
+   FILE* fp = fopen(filename, "r");
+   if(fp == NULL)
+      return;
+
+   int c;
+   while((c = fgetc(fp)) != NULL)
+      putchar(c);
+
+   fclose(fp);
+}
+
+int main(int argc, const char* argv[]) {
+   if(argc < 2)
+      return 0;
+
+   cat(argv[1]);
+  
+
+   return 0;
+}
