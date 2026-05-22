ffece91d19264bed5c049ea7a56b7d5271f835ab
thototh <144813512+thototh@users.noreply.github.com>
2026-05-22 12:34:04 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2) 
-        return 0;
-    
-    FILE* fp = fopen(argv[1], "r");
+   if(argc < 2)
+      return 0;
 
+   FILE* fp = fopen(argv[1], "r");
+   if(fp==NULL){
+      perror("file dose not exists");
+      return 1;
+   }
 
-    return 0;
+   return 0;
 }
