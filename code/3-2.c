f97fe3eefe54eeaae2084fa1f3ddf5bbda0dd518
thototh <144813512+thototh@users.noreply.github.com>
2026-05-23 16:02:36 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main(int argc, const char* argv[]) {
     if(argc < 2)
-      return 0;
+        return 0;
 
-   const char* filename = argv[1];
-   char buffer[10] = {0};
+    const char* filename = argv[1];
+    char buffer[10] = {0};
 
-   FILE* fp = fopen(filename, "r");
-   if(fp == NULL)
-      return 0;
+    FILE* fp = fopen(filename, "r");
+    if(fp == NULL)
+        return 0;
 
-   fscanf(fp, "%s", buffer);
-   fclose(fp);
+    fgets(buffer, sizeof(buffer), fp);
 
-   printf("%s\n", buffer);
+    fclose(fp);
+
+    printf("%.9s\n", buffer);
 
     return 0;
 }
