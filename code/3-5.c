f4fbe519c837deb264a2955fbdfe90c4de25b0fb
thototh <144813512+thototh@users.noreply.github.com>
2026-05-23 16:30:50 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
@@ -8,9 +9,19 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
+    char buffer[256];
     int sum = 0;
 
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        char* end;
+        long val = strtol(buffer, &end, 10);
+
+        if (end == buffer || (*end != '\n' && *end != '\0')) {
+            fprintf(stderr, "invalid input %s", buffer);
+        } else {
+            sum += val;
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
