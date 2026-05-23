ae48a144eff4ddd590f3fe0c9a61394673905df2
thototh <144813512+thototh@users.noreply.github.com>
2026-05-23 16:28:01 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -10,18 +10,32 @@ typedef struct config {
 } Config;
 
 void config_parser(Config* config_ptr) {
+    FILE* fp = fopen("config.txt", "r");
+    char key[64];
+    char value[64];
 
+    while (fscanf(fp, "%[^=]=%s\n", key, value) != EOF) {
+        if (strcmp(key, "InputFileName") == 0) {
+            strcpy(config_ptr->InputFileName, value);
+        }else if (strcmp(key, "Options") == 0) {
+            config_ptr->Options = atoi(value);
+        }else if (strcmp(key, "SectionName") == 0) {
+            strcpy(config_ptr->SectionName, value);
+        }else if (strcmp(key, "Address") == 0) {
+            config_ptr->Address = strtoull(value, NULL, 0);
+        }
+    }
+    fclose(fp);
 }
 
 int main(int argc, const char* argv[]) {
     Config config;
     config_parser(&config);
 
-    printf("config: %s %d %s %llu\n", 
-        config.InputFileName, 
-        config.Options,
-        config.SectionName,
-        config.Address);
+    printf("config: %s %d %s %llu\n",
+           config.InputFileName,
+           config.Options,
+           config.SectionName,
+           config.Address);
     return 0;
 }
-
