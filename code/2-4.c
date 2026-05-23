2d65a4124754de24425cde70c601ec7a130dbadd
thototh <144813512+thototh@users.noreply.github.com>
2026-05-23 15:45:03 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,8 +6,18 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    int alpha[26] = {0};
+    char c;
 
+    while (fgetc(fp) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            alpha[c - 'a']++;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, alpha[i]);
+    }
 
     fclose(fp);
 }
-
